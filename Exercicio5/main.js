dd45adb16fa39ef65785e03f960e769b7319ff8c
GuiF-R <guilhermerht@hotmail.com>
2023-09-12 00:37:57 -0300

    ex5 ok

--- Exercicio5/main.js
+++ Exercicio5/main.js
@@ -1,7 +1,7 @@
-// Defina as variáveis com os valores fornecidos
-const numeroTotalPrestacoes = 120; // Número total de prestações
-const quantidadePrestacoesPagas = 24; // Quantidade de prestações pagas
-const valorPrestacaoAtual = 500; // Valor atual da prestação
+
+const numeroTotalPrestacoes = 120; 
+const quantidadePrestacoesPagas = 24; 
+const valorPrestacaoAtual = 500; 
 
 // Calcule o total pago pelo consorciado
 const totalPago = quantidadePrestacoesPagas * valorPrestacaoAtual;
@@ -10,5 +10,5 @@ const totalPago = quantidadePrestacoesPagas * valorPrestacaoAtual;
 const saldoDevedor = (numeroTotalPrestacoes - quantidadePrestacoesPagas) * valorPrestacaoAtual;
 
 // Exiba os resultados
-console.log("Total pago pelo consorciado: R$" + totalPago.toFixed(2));
-console.log("Saldo devedor: R$" + saldoDevedor.toFixed(2));
+console.log("Valor ja pago pelo consorciado: R$" + totalPago.toFixed(2));
+console.log("Valor que o consorciado ainda deve: R$" + saldoDevedor.toFixed(2));
